1b3708be42da7e11e8c5d31f21985283abe6338d
Monu Carpenter <monuc1315@gmail.com>
2026-02-06 03:30:31 +0530

    feat(auto): added auto save logic/plugin

--- lua/plugins/editor.lua
+++ lua/plugins/editor.lua
@@ -1,4 +1,37 @@
 return {
+{
+  "Pocco81/auto-save.nvim",
+  event = "BufReadPre",
+  config = function()
+    require("auto-save").setup {
+      enabled = true,
+      debounce_delay = 1000, -- 1 second delay, adjust as needed
+      execution_message = {
+        message = function()
+          return ("AutoSave: saved at " .. vim.fn.strftime("%H:%M:%S"))
+        end,
+        dim = 0.18,
+        cleaning_interval = 1250,
+      },
+      trigger_events = {"InsertLeave", "TextChanged"},
+      condition = function(buf)
+        local fn = vim.fn
+        local utils = require("auto-save.utils.data")
+        if fn.getbufvar(buf, "&modifiable") == 1 and utils.not_in(fn.getbufvar(buf, "&filetype"), {}) then
+          return true
+        end
+        return false
+      end,
+      write_all_buffers = false,
+      callbacks = {
+        after_saving = function()
+          vim.cmd("silent! FormatWrite")
+        end,
+      },
+    }
+  end,
+},
+
 	{
 		enabled = false,
 		"folke/flash.nvim",
